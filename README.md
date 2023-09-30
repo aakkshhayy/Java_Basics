6b5931b3706f4ac765267c23f23369abd7cf6253
aakkshhayy <aakkshhayy@gmail.com>
2023-09-30 15:54:33 +0530

    Created a jar file
    Signed-off-by: aakkshhayy <aakkshhayy@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Java_Basics
+#Created a jar file 
+#Created a simple program
---
+++ src/main/java/org/Project/greetings.java
@@ -0,0 +1,7 @@
+package org.Project;
+
+public class greetings {
+    public static void main(String[] args) {
+        System.out.println("Hey Greetings");
+    }
+}
